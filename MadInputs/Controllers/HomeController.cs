b329764b8c3c81c7b3e87dc31335daa5a744d6f1
Entity-903 <charlesmassie021@gmail.com>
2024-07-08 09:23:55 -0600

    Add project files.

---
+++ MadInputs/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using MadInputs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MadInputs.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Input()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult OutPut(string Input1, string Input2, string Input3, string Input4, string Input5)
+		{
+			ViewBag.First = Input1; 
+			ViewBag.Second = Input2; 
+			ViewBag.Third = Input3; 
+			ViewBag.Fourth = Input4; 
+			ViewBag.Fifth = Input5;
+
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
