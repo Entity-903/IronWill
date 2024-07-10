9706876f6f8e1797227c0d5d807ad7d5e8855fa3
Entity-903 <charlesmassie021@gmail.com>
2024-07-10 10:02:36 -0600

    Created Routing and VideoGameLibrary projects.

---
+++ SendThreeWays/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SendThreeWays.Models;
+
+namespace SendThreeWays.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult AlternateViews()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		public IActionResult RouteTest(int? id)
+		{
+			return Content($"id = {id?.ToString() ?? "NULL"}");
+		}
+
+		public IActionResult ParamTest(int? id, string s)
+		{
+			return Content($"id = {id?.ToString() ?? "NULL"} {s}");
+			//id? = nullable variable
+			//?? = null coalsescing operator.  if variable is true do left, else do right
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
