9d184b0b825b429037b4ed3f6199c641d03964c5
Entity-903 <charlesmassie021@gmail.com>
2024-07-16 19:02:35 -0600

    Routing is complete

--- Routing/Controllers/HomeController.cs
+++ Routing/Controllers/HomeController.cs
@@ -1,29 +1,66 @@
 ﻿using System.Diagnostics;
+using System.IO.Pipelines;
 using Microsoft.AspNetCore.Mvc;
 using Routing.Models;
 
 namespace Routing.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult CatchAll()
+        {
+            return View("Index");
+        }
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("/{value:int}")]
+        public IActionResult DefaultRoute(int value)
+        {
+            return Content($"The cow Default Cow moos at you {value} times.");
+        }
+
+        [Route("/EatMoreChicken")]
+        public IActionResult EatMoreChicken()
+        {
+            return Redirect("https://www.chick-fil-a.com/");
+        }
+
+        [Route("/{value:int}/{name}")]
+        public IActionResult Betsy(int value, string name)
+        {
+            return Content($"The cow {name} moos at you {value} times.");
+        }
+
+        [Route("/AllCows/Gallery/{value:int}")]
+        public IActionResult OnThisPage(int value)
+        {
+            return Content($"There are {value} Cows on page.");
+        }
+
+        [Route("/AllCows/Gallery/5/Page8")]
+        public IActionResult ForEveryPage()
+        {
+            return Content($"There are 5 cows per page, on page 8");
+        }
+
+        [Route("/AllCows/Gallery/{value1:int}/{value2:int}")]
+        public IActionResult ForEveryPage(int value1, int value2)
+        {
+            return Content($"There are {value1} cows per page, on page {value2}");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- Routing/Program.cs
+++ Routing/Program.cs
@@ -1,32 +1,77 @@
 namespace Routing
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews();
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
 
-			var app = builder.Build();
+            var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Home/Error");
-			}
-			app.UseStaticFiles();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
 
-			app.UseRouting();
+            app.UseRouting();
 
-			app.UseAuthorization();
+            app.UseAuthorization();
 
-			app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Privacy}/{id?}");
 
-			app.Run();
-		}
-	}
+            app.MapControllerRoute(
+                name: "CatchAll",
+                pattern: "{*any}",
+                defaults: new { controller = "Home", action = "CatchAll"});
+
+            //app.MapControllerRoute(
+            //    name: "DefaultRoute",
+            //    pattern: "/{id:int}",
+            //    defaults: new { controller = "Home", action = "DefaultRoute"});
+
+
+
+
+
+
+
+
+
+
+
+
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Home}/{action=Privacy}/{id?}");
+
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Home}/{action=Privacy}/{id?}");
+
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Home}/{action=Privacy}/{id?}");
+
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Home}/{action=Privacy}/{id?}");
+
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Home}/{action=Privacy}/{id?}");
+
+            //app.MapControllerRoute(
+            //    name: "default",
+            //    pattern: "{controller=Home}/{action=Privacy}/{id?}");
+
+            app.Run();
+        }
+    }
 }
