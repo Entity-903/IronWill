721a01fa8798fbabd80932f6d547a8c342d3cb1e
Entity-903 <charlesmassie021@gmail.com>
2024-07-22 10:42:59 -0600

    Validation completed

---
+++ Validation/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Validation.Models;
+
+namespace Validation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Input()
+        {
+            return View();
+        }
+
+		[HttpPost]
+		public IActionResult Input(UserInput userInput)
+		{
+            // Check joint nullable for Address fields
+            if ((userInput.Street == null && userInput.City == null && userInput.State == null && userInput.ZipCode == null) ||
+				(userInput.Street != null && userInput.City != null && userInput.State != null && userInput.ZipCode != null))
+            {
+                if (ModelState.IsValid) 
+                { 
+                    return RedirectToAction("Output", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("ZipCode", "Address cannot be incomplete!");
+            }
+
+			return View();
+		}
+
+		public IActionResult Output()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Validation/Models/UserInput.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Numerics;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Validation.Models
+{
+    public class UserInput
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(5, int.MaxValue, ErrorMessage = "This is no place for children")]
+        public int Age { get; set; }
+
+        public string? Street { get; set; }
+
+        public string? City { get; set; }
+
+        public string? State { get; set; }
+
+        public int? ZipCode { get; set; }
+
+        public UserInput() { }
+
+        public UserInput(string name, int age, string? street, string? city, string? state, int? zipCode)
+        {
+            Name = name;
+            Age = age;
+            Street = street;
+            City = city;
+            State = state;
+            ZipCode = zipCode;
+        }
+    }
+}
+
+
+/*
+
+    Name(cannot be blank or null)
+    Age(cannot be less the 5)
+    Address(Is jointly nullable; either all subfields or null or not)
+        Street
+        City
+        State
+        Zip Code
+
+*/
